26ef795002a26876244957698c44b53ec90717ee
Serdar Akin <62178465+pserdarakin@users.noreply.github.com>
2025-02-19 20:01:13 +0000

    add Home and WantedList components; implement MovieCard component for displaying movie details; update App component to use MovieCard

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,15 @@
 import {React, useState, useEffect} from "react";
+import axios from "axios";
+
+// components
 import Header from "./Header";
 import SearchBar from "./SearchBar";
+import MovieCard from "./MovieCard";
+
+// css and styling
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col } from "react-bootstrap";
-import MovieCards from "./MovieCards";
-import "./App.css";
-import axios from "axios";
 
 function App() {
 
@@ -14,7 +18,6 @@ function App() {
   const fetchMovies = async () => {
     try {
       const response = await axios.get("http://localhost:8080/api/movies");
-      console.log("Movies data:", response.data); // Debugging log
       setMovies(response.data); 
     } catch (error) {
       console.error("Error fetching movies:", error);
@@ -26,29 +29,37 @@ function App() {
   }, []);
 
   return (
-    <div className="app-container">
-      <div className="background-image"></div>
-      <Container className="mt-4">
-        <Header />
-      </Container>
+    <>
+      <MovieCard movie={{title: "Serdar's Film", release_date: "2024"}} />
+    </>
+  )
 
-      <Container className="box">
-        <SearchBar />
-      </Container>
-
-      <Container className="d-flex justify-content-center min-vh-100 mt-750px">
-        <Row md={6}>
-          {array.map((user, index) => (
-            user.watched_list.map((movie, movieIndex) => (
-              <Col key={`${index}-${movieIndex}`} md={3}>
-                <MovieCards movie={movie} />
-              </Col>
-            ))
-          ))}
-        </Row>
-      </Container>
-    </div>
-  );
+  
 }
 
 export default App;
+
+// return (
+//   <div className="app-container">
+//     <div className="background-image"></div>
+//     <Container className="mt-4">
+//       <Header />
+//     </Container>
+
+//     <Container className="box">
+//       <SearchBar />
+//     </Container>
+
+//     <Container className="d-flex justify-content-center min-vh-100 mt-750px">
+//       <Row md={6}>
+//         {array.map((user, index) => (
+//           user.watched_list.map((movie, movieIndex) => (
+//             <Col key={`${index}-${movieIndex}`} md={3}>
+//               <MovieCard movie={movie} />
+//             </Col>
+//           ))
+//         ))}
+//       </Row>
+//     </Container>
+//   </div>
+// );
---
+++ client/src/components/MovieCard.jsx
@@ -0,0 +1,42 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function MovieCard({movie}) {
+
+  function onFavoriteClick() {
+    alert("clicked")
+  }
+
+  return <div className="movie-card">
+    <div className="movie-poster">
+      <img src={movie.url} alt={movie.title} />
+      <div className="movie-overlay">
+        <button className="favorite-btn" onClick={onFavoriteClick}>
+          ✅
+        </button>
+      </div>
+    </div>
+    <div className="movie-info">
+      <h3>{movie.title}</h3>
+      <p>{movie.release_date}</p>
+    </div>
+  </div>
+  
+}
+
+export default MovieCard;
+
+
+// return (
+//   <Card style={{ width: '13rem' }}>
+//     {/* <Card.Img variant="top" src="https://www.imdb.com/title/tt0468569/mediaviewer/rm4023877632/?ref_=tt_ov_i" /> */}
+//     <Card.Body>
+//       <Card.Title>Card Title</Card.Title>
+//       <Card.Text>
+//         Some quick example text to build on the card title and make up the
+//         bulk of the card's content.
+//       </Card.Text>
+//       <Button variant="primary">Go somewhere</Button>
+//     </Card.Body>
+//   </Card>
+// );
