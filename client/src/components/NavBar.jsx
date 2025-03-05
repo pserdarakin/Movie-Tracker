ba89cd27493df390951503a5be5ba1d8472784ee
Serdar Akin <62178465+pserdarakin@users.noreply.github.com>
2025-03-05 14:19:30 +0000

    add chromatic dependency; rename Movie App to CineVault in NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
     
     <nav className="navbar">
       <div className="navbar-brand">
-        <Link to="/">Movie App</Link>
+        <Link to="/">CineVault</Link>
       </div>
       <div className="navbar-links">
         <Link to="/" className="nav-link">
