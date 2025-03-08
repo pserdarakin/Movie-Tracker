eb4e6264c002edcaac6fd09a2ff8d0496dd2e0b2
Serdar Akin <62178465+pserdarakin@users.noreply.github.com>
2025-03-08 21:40:23 +0000

    remove ThemeContext; update Navbar styles and structure; add storybook log to .gitignore; modify CSS variables for improved theming

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,17 +3,19 @@ import "../css/NavBar.css";
 
 function NavBar() {
   return (
-    
     <nav className="navbar">
       <div className="navbar-brand">
         <Link to="/">CineVault</Link>
       </div>
       <div className="navbar-links">
-        <Link to="/" className="nav-link">
+        <Link to="/" className="nav-link1">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
           Home
         </Link>
-        <Link to="/favorites" className="nav-link">
-          Favorites
+      
+        <Link to="/favorites" className="nav-link2">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path></svg>
+          Watchlist
         </Link>
       </div>
     </nav>
